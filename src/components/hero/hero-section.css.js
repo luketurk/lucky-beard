00c756a4bdab6cac959d9dbbab87168f3f994e2a
luketurk <luketurk101@gmail.com>
2024-03-10 21:13:32 +0200

    adjust vh on diff breakpoints to make curve sit correctly

--- src/components/hero/hero-section.css.js
+++ src/components/hero/hero-section.css.js
@@ -4,37 +4,63 @@ import heroImage from '../../assets/images/hero-image.jpg';
 
 export const Hero = styled.div`
   background-image: url(${heroImage});
-  background-size: cover;
+  background-size: cover; 
   background-position: center;
   height: 100vh; 
   display: flex;
   align-items: center; 
+  flex-direction: column;
   justify-content: center; 
   color: white;
   text-align: center;
   position: relative;
   padding: 15px;
-  z-index:-2;
+  z-index: -2; 
   
   @media (max-width: 768px) {
-    height: 50vh; 
+    height: 70vh; 
+    padding: 10px;
   }
   
   @media (max-width: 480px) {
-    height: 30vh; 
-    background-size: auto 100%; 
+    height: 90vh; 
+    background-size: cover; 
+    background-repeat: no-repeat;
+    margin-bottom:100px;
   }
 `;
 
 export const Title = styled.h1`
-  font-size: 3rem; 
+  font-size: 62px; 
 
   @media (max-width: 768px) {
-    font-size: 2rem; 
+    font-size: 46px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 37px; 
+  }
+`;
+
+export const SubTitle = styled.h5`
+  width:583px;
+  font-size: 16px;
+  font-weight:400; 
+  line-height: 22.4px;
+
+  @media (max-width: 768px) {
+    width:auto;
+    font-size: 1rem; 
   }
   
   @media (max-width: 480px) {
-    font-size: 1.5rem; 
+    font-size: 1rem; 
   }
 `;
+
+export const ButtonWrapper = styled.div`
+  flex:1;
+  justify-content:space-between;
+`;
+
 
