b451b0c34ef39f9ff2b1851803936db7542354be
luketurk <luketurk101@gmail.com>
2024-03-10 21:12:22 +0200

    remove unused import

--- src/components/feature-section/feature-section.js
+++ src/components/feature-section/feature-section.js
@@ -1,34 +1,34 @@
 import React from 'react'
 
-import { FeatureItem, FeatureList, FeatureSectionContainer, ImageContainer,  SectionContainer,  SectionHeading, SectionSubText, TextContainer } from './feature-section.css'
+import { FeatureItem, FeatureList, FeatureSectionContainer, ImageContainer, SectionContainer, SectionHeading, SectionSubText, TextContainer } from './feature-section.css'
 
-import { FeatureIcon } from '../../assets/images/icon.svgs'
+import { FeatureIcon, WhiteFeatureIcon } from '../../assets/images/icon.svgs'
 import FeatureImage from '../../assets/images/feature-image.png';
 
 
 
-export default function FeatureSection({ heading = '', subtext = '', features = [], imgPosition='left', spacing }) {
+export default function FeatureSection({ heading = '', subtext = '', features = [], imgPosition = 'left', spacing }) {
     return (
         <FeatureSectionContainer spacing={spacing} imgPosition={imgPosition}>
             <ImageContainer>
                 <img src={FeatureImage} alt="Description" />
             </ImageContainer>
             <SectionContainer>
-            <TextContainer>
-            <SectionHeading>{heading}</SectionHeading>
-            <SectionSubText>{subtext}</SectionSubText>
-            <FeatureList>
-                {features.map((feature, index) => (
-                    <FeatureItem key={index}>
-                        <FeatureIcon />
-                        {feature}
-                    </FeatureItem>
-                ))}
-            </FeatureList>
-            </TextContainer>
+                <TextContainer>
+                    <SectionHeading>{heading}</SectionHeading>
+                    <SectionSubText>{subtext}</SectionSubText>
+                    <FeatureList>
+                        {features.map((feature, index) => (
+                            <FeatureItem key={index}>
+                                <WhiteFeatureIcon />
+                                {feature}
+                            </FeatureItem>
+                        ))}
+                    </FeatureList>
+                </TextContainer>
             </SectionContainer>
-           
 
-         </FeatureSectionContainer>
+
+        </FeatureSectionContainer>
     )
 }
