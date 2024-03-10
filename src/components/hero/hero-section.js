cefbdfce1beea60c56d43c57da1f3d1ea705c268
luketurk <luketurk101@gmail.com>
2024-03-10 21:13:41 +0200

    add in copy

--- src/components/hero/hero-section.js
+++ src/components/hero/hero-section.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Hero, Title } from './hero-section.css';
+import { ButtonWrapper, Hero, SubTitle, Title } from './hero-section.css';
+import { AppleStoreButtonSVG, ArrowIcon, GooglePlaystoreButtonSVG } from '../../assets/images/icon.svgs';
 
 const HeroSection = () => {
   return (
     <>
         <Hero>
         <Title>A New Era for Test-Drives</Title>
+        <SubTitle>
+        Enhanced test drive experience through the connection of people in a secure, scheduled and convenient manner to ensure the seamless buying and selling of new and used vehicles. 
+        </SubTitle>
+        <ButtonWrapper>
+        <AppleStoreButtonSVG/>
+        <GooglePlaystoreButtonSVG/>
+        </ButtonWrapper>
+
       </Hero>
     </>
   );
