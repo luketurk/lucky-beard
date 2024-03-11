d0d647a34206e1f96c8950d78b81441843c5b31c
luketurk <luketurk101@gmail.com>
2024-03-11 22:01:08 +0200

    add navbar

--- src/components/hero/hero-section.js
+++ src/components/hero/hero-section.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ButtonWrapper, Hero, SubTitle, Title } from './hero-section.css';
 import { AppleStoreButtonSVG, ArrowIcon, GooglePlaystoreButtonSVG } from '../../assets/images/icon.svgs';
+import NavBar from '../navbar/navbar';
 
 const HeroSection = () => {
   return (
     <>
         <Hero>
+          <NavBar/>
         <Title>A New Era for Test-Drives</Title>
         <SubTitle>
         Enhanced test drive experience through the connection of people in a secure, scheduled and convenient manner to ensure the seamless buying and selling of new and used vehicles. 
