5cd8d8b21774437749b3ff73ad34c70edcf23a7a
luketurk <luketurk101@gmail.com>
2024-03-11 21:59:37 +0200

    add contact us route and page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; 
 import LandingPage from './pages/landing/landing.page'
+import DemoRequestPage from './pages/contact/contact-us.page';
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/contact" element={<DemoRequestPage />} />
       </Routes>
     </Router>
   );
