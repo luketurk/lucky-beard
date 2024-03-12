1c163c6c370de804f24443f7b4657cd366aedf1b
luketurk <luketurk101@gmail.com>
2024-03-12 13:24:31 +0200

    add footer and styles

---
+++ src/components/footer/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FooterContainer, FooterContent } from './footer.styles';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <span>Dealr Automotive Limited</span>
+        <span>|</span>
+        <span>Registration number: 629444</span>
+        <span>|</span>
+        <span>15 Harcourt street, St. Kevin's, Dublin 2 D02 XY47, Dublin, Ireland</span>
+      </FooterContent>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/footer.styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  padding: 1rem 2rem;
+  background-color: #6F7889; 
+  color: white;
+  bottom:0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px; 
+`;
+
+export const FooterContent = styled.div`
+  display: flex;
+  gap: 10px; 
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
