fbc0dfb3617687d51bea3b671a21199d3621479b
luketurk <luketurk101@gmail.com>
2024-03-12 13:24:54 +0200

    remove navbar freom hero

--- src/components/hero/hero-section.js
+++ src/components/hero/hero-section.js
@@ -7,7 +7,6 @@ const HeroSection = () => {
   return (
     <>
         <Hero>
-          <NavBar/>
         <Title>A New Era for Test-Drives</Title>
         <SubTitle>
         Enhanced test drive experience through the connection of people in a secure, scheduled and convenient manner to ensure the seamless buying and selling of new and used vehicles. 
