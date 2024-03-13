db30a47e6147de82dbc2f495173bfd375d3d1e40
luketurk <luketurk101@gmail.com>
2024-03-13 09:58:40 +0200

    add in non interactive footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FooterContainer, FooterContent } from './footer.styles';
+import React from "react";
+import { FooterContainer, FooterContent } from "./footer.styles";
 
 const Footer = () => {
   return (
@@ -9,7 +9,9 @@ const Footer = () => {
         <span>|</span>
         <span>Registration number: 629444</span>
         <span>|</span>
-        <span>15 Harcourt street, St. Kevin's, Dublin 2 D02 XY47, Dublin, Ireland</span>
+        <span>
+          15 Harcourt street, St. Kevin's, Dublin 2 D02 XY47, Dublin, Ireland
+        </span>
       </FooterContent>
     </FooterContainer>
   );
--- src/components/footer/footer.styles.js
+++ src/components/footer/footer.styles.js
@@ -2,20 +2,20 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
   padding: 1rem 2rem;
-  background-color: #6F7889; 
+  background-color: #6f7889;
   color: white;
-  bottom:0;
+  bottom: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  font-family: 'Poppins', sans-serif;
-  font-size: 14px; 
+  font-family: "Poppins", sans-serif;
+  font-size: 13px;
 `;
 
 export const FooterContent = styled.div`
   display: flex;
-  gap: 10px; 
+  gap: 10px;
 
   @media (max-width: 768px) {
     flex-direction: column;
