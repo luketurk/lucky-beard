b05dbcd2fefedc77e2731043ef8743ec4464c57b
luketurk <luketurk101@gmail.com>
2024-03-13 10:00:46 +0200

    finalize form styling

--- src/pages/contact/contact-us.styles.js
+++ src/pages/contact/contact-us.styles.js
@@ -1,47 +1,66 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { DownArrowSVG } from '../../assets/images/icon.svgs';
+import DownArrowIcon from "../../assets/images/DownArrowIcon.png";
+
+import { FaChevronDown } from 'react-icons/fa';
 
 export const PageLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100%;
   height: 100vh;
-  background-color: #5A89EA; 
+  background-color: #5a89ea;
+
+  @media (max-width: 576px) {
+    height: 100%;
+  }
 `;
 
 export const Header = styled.header`
   width: 100%;
-  padding: 1rem;
-  background-color: #ffffff; 
+  background-color: #ffffff;
 `;
 
 export const MainSection = styled.main`
   flex-grow: 1;
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
-  background-color: #5A8EA; 
+  background-color: #5A8EA;
+`;
 
+export const TitleContainer = styled.div`
+  text-align: center;
+  margin-bottom: 2rem;
 `;
 
 export const FormContainer = styled.div`
   background-color: #ffffff;
   padding: 2rem;
   border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
-  width: 100%;
-  max-width: 400px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 80%;
+
+  @media (max-width: 768px) {
+    padding: 1rem; 
+  }
+  @media (max-width: 576px) {
+    margin-bottom: 200px;
+    width: 75%;
+  }
 `;
 
 export const FormTitle = styled.h2`
   color: #333333;
   margin-bottom: 1rem;
+  font-size: 24px;
 `;
 
 export const FormSubtitle = styled.p`
-  color: #666666; 
+  color: #666666;
   margin-bottom: 2rem;
 `;
 
@@ -52,67 +71,97 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
-  padding: 0.5rem; 
-  width: 100%; 
-  border: 0px solid #ccc; 
-  border-bottom: 1px solid #ccc; 
-  border-radius: 0px; 
-  margin-bottom: 1rem; 
+  padding: 0.5rem;
+  width: 100%;
+  border: 0px;
+  border-bottom: 1px solid #ccc;
+  border-radius: 0px;
+  margin-bottom: 1rem;
 
-  color: #6F7889; 
-  font-family: 'Poppins', sans-serif;
-  font-size: 27.425px; 
-  font-weight: 400; 
-  line-height: 32.91px; 
+  color: #6f7889;
+  font-family: "Poppins", sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 32.91px;
 
-  
-  ::placeholder {
-    color: #6F7889; 
+  &&::-webkit-input-placeholder {
+    color: #6f7889;
+    font-size: 12px;
   }
-
-
+  &::-moz-placeholder {
+    color: #6f7889;
+    opacity: 1;
+  }
+  &:-ms-input-placeholder {
+    color: #6f7889;
+  }
+  &:-moz-placeholder {
+    color: #6f7889;
+    opacity: 1;
+  }
 
   &:focus {
     outline: none;
-    border-color: #5A89EA;
+    border-color: #5a89ea;
   }
 `;
+
+export const SelectWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const Chevron = styled(FaChevronDown)`
+  position: absolute;
+  right: 1rem;
+  top: 50%;
+  transform: ${({ isRotated }) => isRotated ? 'translateY(-50%) rotate(180deg)' : 'translateY(-50%) rotate(0deg)'};
+  transition: transform 0.3s ease-in-out;
+  pointer-events: none; // Prevent the chevron from blocking clicks on the select
+`;
+
 
 export const Select = styled.select`
-  padding: 0.5rem 2rem 0.5rem 0.5rem; 
-  border: 1px solid #ccc;
-  border-radius: 8px;
+  padding: 0.5rem 2rem 0.5rem 0.5rem; // Right padding increased to make space for Chevron
+  border: 0px solid #ccc;
+  border-bottom: 1px solid #ccc;
   margin-bottom: 1rem;
   appearance: none; 
-  background: url(${DownArrowSVG}) no-repeat right 1rem center;
-  background-size: 1.5rem; 
+  width: 100%; 
+  font-family: "Poppins", sans-serif;
 
   &:focus {
     outline: none;
-    border-color: #5A89EA;
-    box-shadow: 0 0 0 2px rgba(90, 137, 234, 0.5);
+    border-color: #5a89ea;
   }
 `;
 
+//
 
+
+export const BlackBanner = styled.div`
+  background: var(--Dark-1, #030303);
+  height: 100px;
+  width: 100%;
+`;
 
 export const SubmitButton = styled.button`
   padding: 0.75rem;
   border: none;
   border-radius: 4px;
-  background-color: #5A89EA;
+  background-color: #5a89ea;
   color: white;
   font-weight: bold;
   cursor: pointer;
-  
+
   &:hover {
     background-color: #4877c2;
   }
 
   &:disabled {
-    background-color: #AAB4BE; 
-    cursor: not-allowed; 
-    opacity: 0.5; 
+    background-color: #aab4be;
+    cursor: not-allowed;
+    opacity: 0.5;
   }
 `;
 
@@ -122,4 +171,62 @@ export const Footer = styled.footer`
   color: white;
   width: 100%;
   text-align: center;
+`;
+
+export const ErrorMessage = styled.p`
+  color: #ff6565;
+  font-family: "Poppins", sans-serif;
+  font-size: 14px;
+  margin: -1rem 0 1rem 0;
+  animation: fadeIn 0.3s ease-in-out;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+export const ContactUsTitle = styled.h1`
+  color: #fff;
+  text-align: center;
+  font-family: "Poppins", sans-serif;
+  font-size: 62px;
+  font-weight: 600;
+  line-height: 105%; 
+  letter-spacing: -1.86px;
+
+  @media (max-width: 768px) {
+    font-size: 42px;
+    line-height: normal;
+    letter-spacing: normal;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 24px; 
+    line-height: normal;
+    letter-spacing: normal;
+    margin-top: 100px;
+  }
+`;
+
+export const ContactUsSubTitle = styled.h5`
+  width: 583px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22.4px;
+  color: white;
+
+  @media (max-width: 768px) {
+    width: auto;
+    font-size: 1rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 0.8rem;
+    padding: 30px;
+  }
 `;
