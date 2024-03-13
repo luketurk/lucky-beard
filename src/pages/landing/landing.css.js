8d08a2a303de27e0e26be7929fcc2db46833dbbc
luketurk <luketurk101@gmail.com>
2024-03-13 10:02:35 +0200

    finalize landing page css and add sections with relevant ids

--- src/pages/landing/landing.css.js
+++ src/pages/landing/landing.css.js
@@ -8,41 +8,39 @@ export const PageContainer = styled.div`
 export const CurveWrapper = styled.div`
   position: absolute;
   width: 100%;
-  top: 80vh; 
+  top: 80vh;
   left: 0;
   right: 0;
   overflow: hidden;
   z-index: 3;
 
-
   svg {
     width: 100%;
     height: auto;
-    display: block; 
-    preserveAspectRatio: xMidYMid meet;
+    display: block;
+    preserveaspectratio: xMidYMid meet;
   }
 
   @media (max-width: 768px) {
-    top: 60vh; 
-
+    top: 60vh;
   }
-  
+
   @media (max-width: 480px) {
-    top: 85vh; 
-    background-size: auto 100%; 
+    top: 85vh;
+    background-size: auto 100%;
   }
 `;
 
 export const InnerCurveWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100%; 
+  height: 100%;
 `;
 
 export const StyledArrowIcon = styled(ArrowIcon)`
   position: absolute;
   left: 50%;
   transform: translate(-50%, 50%);
-  width: 50px; 
+  width: 50px;
   height: auto;
 `;
--- src/pages/landing/landing.page.jsx
+++ src/pages/landing/landing.page.jsx
@@ -1,46 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import FeatureSection from '../../components/feature-section/feature-section'
-import HeroSection from '../../components/hero/hero-section'
-import TrustBuilderSection from '../../components/trust-builder-section/trust-builder-section';
+import FeatureSection from "../../components/feature-section/feature-section";
+import HeroSection from "../../components/hero/hero-section";
+import TrustBuilderSection from "../../components/trust-builder-section/trust-builder-section";
 
-import { CombinedSVG, CurveBackground } from '../../assets/curve-background.svg';
-import { CurveWrapper,  PageContainer  } from './landing.css';
-
-
+import {
+  CombinedSVG,
+  CurveBackground,
+} from "../../assets/curve-background.svg";
+import { CurveWrapper, PageContainer } from "./landing.css";
+import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
 
 export default function LandingPage() {
   return (
     <>
+      <NavBar />
       <PageContainer>
         <HeroSection />
         <CurveWrapper>
-          <CombinedSVG/>
+          <CombinedSVG />
         </CurveWrapper>
-        <FeatureSection
-          heading="Search for the car of your dreams and test drive it"
-          subtext="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed malesuada habitant odio eu etiam proin odio habitant. Elit felis aliquam suspendisse amet nunc. Erat."
-          features={[
-            "Multiple filtering options",
-            "Test drives with both private sellers or dealerships",
-            "Take pictures and notes during test drive"
-          ]}
-        />
-
+        <section id="for-buyers">
+          <FeatureSection
+            heading="Search for the car of your dreams and test drive it"
+            subtext="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed malesuada habitant odio eu etiam proin odio habitant. Elit felis aliquam suspendisse amet nunc. Erat."
+            features={[
+              "Multiple filtering options",
+              "Test drives with both private sellers or dealerships",
+              "Take pictures and notes during test drive",
+            ]}
+          />
+        </section>
         <FeatureSection
           heading="Only real people"
           subtext="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu sed malesuada habitant odio eu etiam proin odio habitant. Elit felis aliquam suspendisse amet nunc. Erat."
           features={[
             "Secure and verified registration",
             "Direct messaging with the seller",
-            "Instant armed response if something goes wrong"
+            "Instant armed response if something goes wrong",
           ]}
-          imgPosition='right'
-
+          imgPosition="right"
         />
-        <TrustBuilderSection/>
+        <section id="for-sellers">
+          <TrustBuilderSection />
+        </section>
       </PageContainer>
-
+      <Footer />
     </>
-  )
+  );
 }
