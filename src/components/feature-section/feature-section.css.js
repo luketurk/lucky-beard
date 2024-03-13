313224a87b48e798ea912d85322025862a4a7063
luketurk <luketurk101@gmail.com>
2024-03-13 09:56:21 +0200

    update styling on feature section to match design

--- src/components/feature-section/feature-section.css.js
+++ src/components/feature-section/feature-section.css.js
@@ -2,76 +2,102 @@ import styled from "styled-components";
 
 export const SectionContainer = styled.section`
   color: white;
-  padding: 40px; 
-  border-radius: 8px; 
-  margin: 20px; 
-  
-  max-width: 400px; 
-  text-align: center; 
+  padding: 40px;
+  border-radius: 8px;
+  margin: 20px;
+  max-width: 400px;
+  text-align: center;
+  @media (max-width: 768px) {
+    padding: 20px;
+    margin: 20px 10px;
+  }
 `;
 export const SectionHeading = styled.h2`
-  font-size: 1.8em; 
-  margin-bottom: 0.5em; 
+  font-size: 1.8em;
+  margin-bottom: 0.5em;
   text-align: left;
-  
+
+  @media (max-width: 768px) {
+    font-size: 1.3em;
+  }
 `;
 
 export const SectionSubText = styled.p`
-  margin-bottom: 2em; 
+  margin-bottom: 2em;
   text-align: left;
-  
-  font-size: 0.7em; 
+  font-size: 0.7em;
 
+  @media (max-width: 768px) {
+    font-size: 0.6em;
+  }
 `;
 
 export const FeatureList = styled.ul`
-  list-style: none; 
-  padding: 0; 
+  list-style: none;
+  padding: 0;
+`;
+
+export const StyledIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const FeatureTextContainer = styled.div`
+  margin-left: 16px;
+  text-align: left;
+  flex: 1;
 `;
 
 export const FeatureItem = styled.li`
-  font-size: 0.7em; 
-  margin-bottom: 1em; 
+  font-size: 0.8em;
+  margin-bottom: 1em;
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  gap:10px;
+
+  @media (max-width: 768px) {
+    font-size: 0.6em;
+  }
 `;
 
 export const ImageContainer = styled.div`
   img {
-    width: 100%; 
+    width: 100%;
     max-width: 200px;
-    height: auto; 
+    height: auto;
 
-    @media (min-width: 768px) { 
-      max-width: 100%; 
-      height: auto; 
+    @media (min-width: 768px) {
+      max-width: 100%;
+      height: auto;
     }
 
-    @media (min-width: 1024px) { 
-      max-width: 400px; 
-      height: auto; 
+    @media (min-width: 1024px) {
+      max-width: 400px;
+      height: auto;
     }
   }
 `;
 
-
 export const TextContainer = styled.div`
   flex: 1;
-  margin-bottom:100px;
+  margin-bottom: 100px;
 `;
 
 export const FeatureSectionContainer = styled.div`
-  background: #5A89EA;
-  position:relative;
-  z-index: 4; 
-  top:0;
+  background: #5a89ea;
+  position: relative;
+  z-index: 4;
+  top: 0;
   display: flex;
-  flex-direction: ${({ imgPosition }) => imgPosition === 'left' ? 'row' : 'row-reverse'};
+  flex-direction: ${({ imgPosition }) =>
+    imgPosition === "left" ? "row" : "row-reverse"};
   align-items: center;
-  justify-content: center; 
-  padding: ${({ spacing }) => spacing || '50px'}; 
+  justify-content: center;
+  padding: ${({ spacing }) => spacing || "50px"};
 
   @media (max-width: 768px) {
     flex-direction: column;
--- src/components/feature-section/feature-section.js
+++ src/components/feature-section/feature-section.js
@@ -1,34 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import { FeatureItem, FeatureList, FeatureSectionContainer, ImageContainer, SectionContainer, SectionHeading, SectionSubText, TextContainer } from './feature-section.css'
+import {
+  FeatureItem,
+  FeatureList,
+  FeatureSectionContainer,
+  FeatureTextContainer,
+  ImageContainer,
+  SectionContainer,
+  SectionHeading,
+  SectionSubText,
+  StyledIcon,
+  TextContainer,
+} from "./feature-section.css";
 
-import {  WhiteFeatureIcon } from '../../assets/images/icon.svgs'
-import FeatureImage from '../../assets/images/feature-image.png';
+import { WhiteFeatureIcon } from "../../assets/images/icon.svgs";
+import FeatureImage from "../../assets/images/feature-image.png";
 
-
-
-export default function FeatureSection({ heading = '', subtext = '', features = [], imgPosition = 'left', spacing }) {
-    return (
-        <FeatureSectionContainer spacing={spacing} imgPosition={imgPosition}>
-            <ImageContainer>
-                <img src={FeatureImage} alt="Description" />
-            </ImageContainer>
-            <SectionContainer>
-                <TextContainer>
-                    <SectionHeading>{heading}</SectionHeading>
-                    <SectionSubText>{subtext}</SectionSubText>
-                    <FeatureList>
-                        {features.map((feature, index) => (
-                            <FeatureItem key={index}>
-                                <WhiteFeatureIcon />
-                                {feature}
-                            </FeatureItem>
-                        ))}
-                    </FeatureList>
-                </TextContainer>
-            </SectionContainer>
-
-
-        </FeatureSectionContainer>
-    )
+export default function FeatureSection({
+  heading = "",
+  subtext = "",
+  features = [],
+  imgPosition = "left",
+  spacing,
+}) {
+  return (
+    <FeatureSectionContainer spacing={spacing} imgPosition={imgPosition}>
+      <ImageContainer>
+        <img src={FeatureImage} alt="Description" />
+      </ImageContainer>
+      <SectionContainer>
+        <TextContainer>
+          <SectionHeading>{heading}</SectionHeading>
+          <SectionSubText>{subtext}</SectionSubText>
+          <FeatureList>
+            {features.map((feature, index) => (
+              <FeatureItem key={index}>
+                <StyledIcon>
+                  <WhiteFeatureIcon />
+                </StyledIcon>
+                <FeatureTextContainer>{feature}</FeatureTextContainer>
+              </FeatureItem>
+            ))}
+          </FeatureList>
+        </TextContainer>
+      </SectionContainer>
+    </FeatureSectionContainer>
+  );
 }
